e64641762c9f18998514b20dfaba2f8274e9d856
Rolando José Garcia Escobar <rolaescobar@gmail.com>
2024-11-02 11:11:24 -0600

    Agregar archivos de proyecto.

---
+++ DTEMVC/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using DTEMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DTEMVC.Controllers
+{
+    public class UserController : Controller
+    {
+       public IActionResult Index()
+        {
+            // Datos de ejemplo para la tabla
+            var users = new List<User>
+            {
+                new User { Id = 1, FirstName = "John", LastName = "Doe", Email = "john.doe@example.com", Role = "Admin" },
+                new User { Id = 2, FirstName = "Jane", LastName = "Smith", Email = "jane.smith@example.com", Role = "User" },
+                new User { Id = 3, FirstName = "Sam", LastName = "Wilson", Email = "sam.wilson@example.com", Role = "User" },
+                 new User { Id = 4, FirstName = "Rolando", LastName = "Jose", Email = "rjgarcia@example.com", Role = "User" },
+            };
+
+            return View(users);
+        }
+    }
+}
---
+++ DTEMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DTEMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
